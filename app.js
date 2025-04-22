1e7b38b5f0cbd33ae959e16b440f4304b070c2a1
toanleviet95 <toanleviet95@gmail.com>
2025-04-23 02:02:18 +0700

    chore: update code and dockerfile

--- app.js
+++ app.js
@@ -56,6 +56,10 @@ app.post("/crawl-linkedin-group", async (req, res) => {
       httpOnly: true,
       secure: true,
     });
+    
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    );
 
     await page.goto(linkedin_group_url, {
       waitUntil: "domcontentloaded",
